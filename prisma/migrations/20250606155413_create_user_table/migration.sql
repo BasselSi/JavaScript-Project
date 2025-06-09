8e7f78fd4b2e9e8a5f96a1250f762be3e087a76a
BasselSi <bassel.sinokrot@gmail.com>
2025-06-09 15:58:36 +0200

    Updated SQLite database

--- prisma/migrations/20250606155413_create_user_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `User` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(20) NOT NULL,
-    `isPremium` BOOLEAN NOT NULL DEFAULT false,
-
-    UNIQUE INDEX `User_name_key`(`name`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
